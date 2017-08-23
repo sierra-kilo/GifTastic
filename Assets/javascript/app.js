4fa405dcb6b49466334d5e5e54f634ab5d09151a
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2017-08-23 14:01:14 -0700

    added lightbox, gives errors

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -13,7 +13,24 @@ $.ajax({
 
     $image.attr("src", imageUrl);
     $image.attr("alt", "image");
+    $image.data(2);
 
     $(".gif-display").html($image);
+    var imageUrl = response.data[2].images.fixed_height_small_still.url;
+    var $image2 = $("<img>");
 
+    $image2.attr("src", imageUrl);
+    $image2.attr("alt", "image2");
+
+    $(".gif-display").append($image2);
+
+    $('img').click(function () {
+        var $gif = $('<img src="' + response.data[2].images.fixed_height_small_still.url + '">');
+        $('lightbox-content').append($gif);
+    });
 });
+
+
+    // <div class='lightbox-content'>
+    //     <img src="image.png">
+    //     <div class="lightbox-caption"><p>Your caption here</p></div>
